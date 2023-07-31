310e105ac1ca7cd318445f870a3e0ffbb1b4d651
Miriam <longoyapusmiriam@gmail.com>
2023-07-31 08:53:48 +0300

    Movies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import GetMovies from './Components/GetMovies';
+import Navbar from './Components/Navbar';
+import Categories from './Components/Categories';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+    
+      <Categories/>
+      <GetMovies/>
     </div>
   );
 }
---
+++ src/Components/Categories/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MovieCategory = ({ category }) => {
+  return <li>{category}</li>;
+};
+
+export default MovieCategory;
